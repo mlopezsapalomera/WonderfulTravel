293315932ae0ce7459ab032e822444ca860d1f85
Alexis Marc Boisset Pérez <alexismarcbp@gmail.com>
2024-11-15 13:16:52 +0100

    Añadir efectos de transición a las imágenes y permitir el cambio entre formato de 12 y 24 horas en el reloj

---
+++ Projecte/scripts/datos.js
@@ -0,0 +1,10 @@
+// scripts/datos.js
+const ofertas = [
+    {
+        nombre: 'Viaje a España',
+        precio: 100.00,
+        imagen: 'images/espana.jpg',
+        fechas: ['2024-12-25', '2025-01-10']
+    },
+    // Más ofertas
+];
